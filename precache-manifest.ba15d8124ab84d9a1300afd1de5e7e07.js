10e354e98a74867be3423e99cab391e86701d863
Antonio <antonio.valverde.balbuena@gmail.com>
2020-11-13 09:00:53 +0100

    Updates

---
+++ precache-manifest.ba15d8124ab84d9a1300afd1de5e7e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e6a7a442821fa97eda726f7ee4c42a",
+    "url": "/reto3/index.html"
+  },
+  {
+    "revision": "f6059c26efb2f94ef873",
+    "url": "/reto3/static/css/main.cf78102c.chunk.css"
+  },
+  {
+    "revision": "c82d3a9d359fd92ff857",
+    "url": "/reto3/static/js/2.513341ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reto3/static/js/2.513341ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6059c26efb2f94ef873",
+    "url": "/reto3/static/js/main.897b295b.chunk.js"
+  },
+  {
+    "revision": "c3823a919fbc145dc1ca",
+    "url": "/reto3/static/js/runtime-main.e6e1209c.js"
+  }
+]);
